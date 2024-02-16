269c564dcb7ef9455d1d73be3c2445cc17a2bbbe
zeid <zeidhassan4@gmail.com>
2024-02-16 09:52:54 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Order from "./Order"
+import Cart from "./Cart";
 
 
 function App() {
   return (
     <div className="App">
       <Order/>
+      <Cart/>
     </div>
   );
 }
